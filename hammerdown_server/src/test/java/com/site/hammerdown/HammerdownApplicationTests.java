f612c74432d0f8a0bb48df1dd89558b501ff1b56
Shyam Rathod <shyam.rathod2601@gmail.com>
2025-02-24 22:41:27 +0530

    feat: initial server setup

---
+++ hammerdown_server/src/test/java/com/site/hammerdown/HammerdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.hammerdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammerdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
